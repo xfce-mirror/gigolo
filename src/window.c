f1709ba2eedc2089d94e3813889127ca48230ede
Enrico TrÃ¶ger <enrico@xfce.org>
2009-04-17 12:20:33 +0000

    Only query the window state at exit if the main window was realised at all.
    (Old svn revision: 7221)

--- src/window.c
+++ src/window.c
@@ -120,10 +120,10 @@ static void gigolo_window_destroy(GigoloWindow *window)
 
 	if (gigolo_settings_get_boolean(priv->settings, "save-geometry"))
 	{
+		GdkWindow *gdk_window = gtk_widget_get_window(GTK_WIDGET(window));
 		gtk_window_get_position(GTK_WINDOW(window), &geo[0], &geo[1]);
 		gtk_window_get_size(GTK_WINDOW(window), &geo[2], &geo[3]);
-		if (gdk_window_get_state(
-				gigolo_widget_get_window(GTK_WIDGET(window))) & GDK_WINDOW_STATE_MAXIMIZED)
+		if (gdk_window != NULL && gdk_window_get_state(gdk_window) & GDK_WINDOW_STATE_MAXIMIZED)
 			geo[4] = 1;
 		else
 			geo[4] = 0;
