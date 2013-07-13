b344b089a1e0a8aea5370df3c6d5bfdbb02caa57
Nick Schermer <nick@xfce.org>
2013-07-13 21:41:32 +0200

    Cleanup a bit.

--- autogen.sh
+++ autogen.sh
@@ -1,77 +1,32 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#
+# vi:set et ai sw=2 sts=2 ts=2: */
+#-
+# Copyright (c) Xfce Development Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-DIE=0
-
-(test -f $srcdir/configure.ac) || {
-	echo "**Error**: Directory "\`$srcdir\'" does not look like the top-level package directory"
-	exit 1
+(type xdt-autogen) >/dev/null 2>&1 || {
+  cat >&2 <<EOF
+autogen.sh: You don't seem to have the Xfce development tools installed on
+            your system, which are required to build this software.
+            Please install the xfce4-dev-tools package first, it is available
+            from http://www.xfce.org/.
+EOF
+  exit 1
 }
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "**Error**: You must have \`autoconf' installed."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "**Error**: You must have \`intltool' installed."
-	echo "You can get it from:"
-	echo "  ftp://ftp.gnome.org/pub/GNOME/"
-	DIE=1
-}
-
-(glib-gettextize --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "**Error**: You must have \`glib' installed."
-	echo "You can get it from: ftp://ftp.gtk.org/pub/gtk"
-	DIE=1
-}
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "**Error**: You must have \`automake' (1.7 or later) installed."
-	echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-	NO_AUTOMAKE=yes
-}
-
-
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-	echo "installed doesn't appear recent enough."
-	echo "You can get automake from ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-if test -z "$*"; then
-	echo "**Warning**: I am going to run \`configure' with no arguments."
-	echo "If you wish to pass any to it, please specify them on the"
-	echo \`$0\'" command line."
-	echo
-fi
-
-echo "Processing configure.ac"
-
-echo "no" | glib-gettextize --force --copy
-intltoolize --copy --force --automake
-aclocal
-autoheader
-automake --add-missing --copy --gnu
-autoconf
-
-echo "Running $srcdir/configure $@ ..."
-$srcdir/configure "$@" && echo "Now type 'make' to compile." || exit 1
-
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen $@
