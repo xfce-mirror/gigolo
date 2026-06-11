xfce-mirror/gigolo

commit 8db9ebb9363931defdb1b2fd1e1aba21f5a02763

gobject-linter: Fix missing_autoptr_cleanup

src/window.c
@@ -29,8 +29,6 @@
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
-#include <gtk/gtk.h>
-#include <gtk/gtk.h>
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
 #define WINDOWING_IS_X11() GDK_IS_X11_DISPLAY (gdk_display_get_default ())
@@ -95,6 +93,11 @@ struct _GigoloWindowPrivate
 	GtkAccelGroup   *accel_group;
 
 	guint			 autoconnect_timeout_id;
+};
+
+struct _GigoloWindow
+{
+	GtkWindow parent;
 };
 
 enum

src/backendgvfs.c
@@ -18,7 +18,6 @@
  */
 
 
-#include <glib-object.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
 #include <gtk/gtk.h>
@@ -84,6 +83,11 @@ struct _GigoloBackendGVFSPrivate
 	GtkListStore *store;
 
 	gint browse_counter;
+};
+
+struct _GigoloBackendGVFS
+{
+	GObject parent;
 };
 
 static void gigolo_backend_gvfs_finalize  			(GObject *object);

src/backendgvfs.h
@@ -21,31 +21,13 @@
 #ifndef __BACKENDGVFS_H__
 #define __BACKENDGVFS_H__
 
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
 G_BEGIN_DECLS
 
 #define GIGOLO_BACKEND_GVFS_TYPE				(gigolo_backend_gvfs_get_type())
-#define GIGOLO_BACKEND_GVFS(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj),\
-			GIGOLO_BACKEND_GVFS_TYPE, GigoloBackendGVFS))
-#define GIGOLO_BACKEND_GVFS_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass),\
-			GIGOLO_BACKEND_GVFS_TYPE, GigoloBackendGVFSClass))
-#define IS_GIGOLO_BACKEND_GVFS(obj)				(G_TYPE_CHECK_INSTANCE_TYPE((obj),\
-			GIGOLO_BACKEND_GVFS_TYPE))
-#define IS_GIGOLO_BACKEND_GVFS_CLASS(klass)		(G_TYPE_CHECK_CLASS_TYPE((klass),\
-			GIGOLO_BACKEND_GVFS_TYPE))
-
-
-typedef struct _GigoloBackendGVFS				GigoloBackendGVFS;
-typedef struct _GigoloBackendGVFSClass			GigoloBackendGVFSClass;
-
-struct _GigoloBackendGVFS
-{
-	GObject parent;
-};
-
-struct _GigoloBackendGVFSClass
-{
-	GObjectClass parent_class;
-};
+G_DECLARE_FINAL_TYPE(GigoloBackendGVFS, gigolo_backend_gvfs, GIGOLO, BACKEND_GVFS, GObject)
 
 typedef struct
 {
@@ -85,7 +67,6 @@ enum
 };
 
 
-GType				gigolo_backend_gvfs_get_type					(void);
 GigoloBackendGVFS*	gigolo_backend_gvfs_new							(void);
 
 gboolean			gigolo_backend_gvfs_is_mount					(gpointer mount);

src/bookmark.c
@@ -19,7 +19,6 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <glib-object.h>
 
 #include "bookmark.h"
 #include "common.h"
@@ -43,6 +42,11 @@ struct _GigoloBookmarkPrivate
 	gboolean should_not_autoconnect;
 
 	gboolean is_valid;
+};
+
+struct _GigoloBookmark
+{
+	GObject parent;
 };
 
 static void gigolo_bookmark_finalize  		(GObject *object);
@@ -267,10 +271,10 @@ GigoloBookmark *gigolo_bookmark_new_from_uri(const gchar *name, const gchar *uri
 
 
 /* Copy the contents of the bookmark 'src' into the existing bookmark 'dest' */
-void gigolo_bookmark_clone(GigoloBookmark *dst, const GigoloBookmark *src)
+void gigolo_bookmark_clone(GigoloBookmark *dst, GigoloBookmark *src)
 {
 	GigoloBookmarkPrivate *priv_dst;
-	const GigoloBookmarkPrivate *priv_src;
+	GigoloBookmarkPrivate *priv_src;
 
 	g_return_if_fail(dst != NULL);
 	g_return_if_fail(src != NULL);

src/bookmark.h
@@ -21,36 +21,19 @@
 #ifndef __BOOKMARK_H__
 #define __BOOKMARK_H__
 
+#include <glib-object.h>
+
 G_BEGIN_DECLS
 
 #define GIGOLO_BOOKMARK_TYPE					(gigolo_bookmark_get_type())
-#define GIGOLO_BOOKMARK(obj)					(G_TYPE_CHECK_INSTANCE_CAST((obj),\
-			GIGOLO_BOOKMARK_TYPE, GigoloBookmark))
-#define GIGOLO_BOOKMARK_CLASS(klass)			(G_TYPE_CHECK_CLASS_CAST((klass),\
-			GIGOLO_BOOKMARK_TYPE, GigoloBookmarkClass))
-#define IS_GIGOLO_BOOKMARK(obj)					(G_TYPE_CHECK_INSTANCE_TYPE((obj), GIGOLO_BOOKMARK_TYPE))
-#define IS_GIGOLO_BOOKMARK_CLASS(klass)			(G_TYPE_CHECK_CLASS_TYPE((klass), GIGOLO_BOOKMARK_TYPE))
+G_DECLARE_FINAL_TYPE(GigoloBookmark, gigolo_bookmark, GIGOLO, BOOKMARK, GObject)
 
-typedef struct _GigoloBookmark					GigoloBookmark;
-typedef struct _GigoloBookmarkClass				GigoloBookmarkClass;
-
-struct _GigoloBookmark
-{
-	GObject parent;
-};
-
-struct _GigoloBookmarkClass
-{
-	GObjectClass parent_class;
-};
-
-GType				gigolo_bookmark_get_type		(void);
 GigoloBookmark*		gigolo_bookmark_new				(void);
 GigoloBookmark*		gigolo_bookmark_new_from_uri	(const gchar *name, const gchar *uri);
 
 gboolean			gigolo_bookmark_is_valid		(GigoloBookmark *bookmark);
 
-void				gigolo_bookmark_clone			(GigoloBookmark *dst, const GigoloBookmark *src);
+void				gigolo_bookmark_clone			(GigoloBookmark *dst, GigoloBookmark *src);
 
 gchar*				gigolo_bookmark_get_uri			(GigoloBookmark *bookmark);
 gchar*				gigolo_bookmark_get_uri_escaped	(GigoloBookmark *bookmark);

src/bookmarkdialog.c
@@ -19,7 +19,6 @@
 
 #include <glib.h>
 #include <glib/gi18n.h>
-#include <gtk/gtk.h>
 
 #include "bookmark.h"
 #include "settings.h"
@@ -44,6 +43,11 @@ struct _GigoloBookmarkDialogPrivate
 	GtkWidget *popup_menu;
 	GtkWidget *edit_item;
 	GtkWidget *delete_item;
+};
+
+struct _GigoloBookmarkDialog
+{
+	GtkDialog parent;
 };
 
 enum

src/bookmarkdialog.h
@@ -21,32 +21,13 @@
 #ifndef __BOOKMARKDIALOG_H__
 #define __BOOKMARKDIALOG_H__
 
+#include <gtk/gtk.h>
+
 G_BEGIN_DECLS
 
 #define GIGOLO_BOOKMARK_DIALOG_TYPE				(gigolo_bookmark_dialog_get_type())
-#define GIGOLO_BOOKMARK_DIALOG(obj)				(G_TYPE_CHECK_INSTANCE_CAST((obj),\
-			GIGOLO_BOOKMARK_DIALOG_TYPE, GigoloBookmarkDialog))
-#define GIGOLO_BOOKMARK_DIALOG_CLASS(klass)		(G_TYPE_CHECK_CLASS_CAST((klass),\
-			GIGOLO_BOOKMARK_DIALOG_TYPE, GigoloBookmarkDialogClass))
-#define IS_GIGOLO_BOOKMARK_DIALOG(obj)			(G_TYPE_CHECK_INSTANCE_TYPE((obj),\
-			GIGOLO_BOOKMARK_DIALOG_TYPE))
-#define IS_GIGOLO_BOOKMARK_DIALOG_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE((klass),\
-			GIGOLO_BOOKMARK_DIALOG_TYPE))
+G_DECLARE_FINAL_TYPE(GigoloBookmarkDialog, gigolo_bookmark_dialog, GIGOLO, BOOKMARK_DIALOG, GtkDialog)
 
-typedef struct _GigoloBookmarkDialog			GigoloBookmarkDialog;
-typedef struct _GigoloBookmarkDialogClass		GigoloBookmarkDialogClass;
-
-struct _GigoloBookmarkDialog
-{
-	GtkDialog parent;
-};
-
-struct _GigoloBookmarkDialogClass
-{
-	GtkDialogClass parent_class;
-};
-
-GType		gigolo_bookmark_dialog_get_type		(void);
 GtkWidget*	gigolo_bookmark_dialog_new			(GigoloWindow *parent);
 
 G_END_DECLS

src/bookmarkeditdialog.c
@@ -22,7 +22,6 @@
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
-#include <gtk/gtk.h>
 
 #include "common.h"
 #include "bookmark.h"
@@ -83,6 +82,11 @@ struct _GigoloBookmarkEditDialogPrivate
 	GigoloBookmark *bookmark_update;
 
 	gulong browse_host_signal_id;
+};
+
+struct _GigoloBookmarkEditDialog
+{
+	GtkDialog parent;
 };
 
 static void gigolo_bookmark_edit_dialog_set_property		(GObject *object, guint prop_id,
@@ -158,7 +162,7 @@ static void gigolo_bookmark_edit_dialog_finalize(GObject *object)
 	GigoloBackendGVFS *backend;
 
 	backend = gigolo_window_get_backend(priv->parent);
-	if (backend != NULL && IS_GIGOLO_BACKEND_GVFS(backend) && priv->browse_host_signal_id > 0)
+	if (backend != NULL && GIGOLO_IS_BACKEND_GVFS(backend) && priv->browse_host_signal_id > 0)
 	{
 		g_signal_handler_disconnect(gigolo_window_get_backend(priv->parent), priv->browse_host_signal_id);
 		priv->browse_host_signal_id = 0;

commit e46f8f72bfd226a639ac2120db5d34c453600dd8

Add missing include
It's needed to know if GDK_WINDING_X11 is defined: tricky…

Fixes: 8db9ebb9363931defdb1b2fd1e1aba21f5a02763

src/window.c
@@ -29,6 +29,7 @@
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
+#include <gdk/gdk.h>
 #ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
 #define WINDOWING_IS_X11() GDK_IS_X11_DISPLAY (gdk_display_get_default ())